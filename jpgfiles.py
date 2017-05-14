63d752c01f669ae830ed7c8205374ac4e519e0c0
Radhika Gulati <radhigulati@gmail.com>
2017-05-13 23:23:01 -0700

    comments for jpg script

--- jpgfiles.py
+++ jpgfiles.py
@@ -1,5 +1,10 @@
+# glob is the module that finds all the pathnames matches a specified pattern
+# os module provides portable way of using OS dependent functionality
 import os, glob
 
+# os.chidr changes to directory given a path
 os.chdir("/Users/radhigulati")
 for file in glob.glob("*.ipynb"):
+# glob.glob returns a possibly-empty list of path names that match the specific
+# pathname
     print(file)
